9bf9a63343f8482d70f2f582a42cacbe00bbdf5e
Mandy <mandysommerfield@gmail.com>
2019-04-04 23:16:23 -0400

    complete todo app

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,18 +1,35 @@
 import React, { Component } from "react";
+import FlipMove from "react-flip-move";
+import "./TodoList.css"
 
 class TodoItems extends Component {
+    constructor(props) {
+        super(props);
+
+        this.createTasks = this.createTasks.bind(this);
+    }
+    
     createTasks(item) {
-        return <li key={item.key}>{item.text}</li>
+        return <li onClick={() => this.delete(item.key)}
+                    key={item.key}>{item.text}</li>
+    }
+
+    delete(key) {
+        this.props.delete(key);
     }
 
     render() {
-        var todoEntries = this.props.entries
+        var todoEntries = this.props.entries;
         var listItems = todoEntries.map(this.createTasks)
 
         return (
-            <ul className="theList">
-            {listItems}
+            <ul className="thisList">
+            <FlipMove duration={250} easing="ease-out">
+                {listItems}
+                </FlipMove>
             </ul>
-        )
+        );
     }
 }
+
+export default TodoItems;
